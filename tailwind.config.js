4e1fcc63d2fb13cce7f554f582b61c2d60aced3c
Helen Shatynska <helen.shatynska@gmail.com>
2023-05-15 22:58:09 +0300

    Add primary color to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          100: "#FFFFFF",
+          200: "#F5F5F5",
+          500: "#646464",
+          600: "#434343",
+          700: "#3A3A3A",
+          800: "#303030",
+          900: "#222222",
+        },
       },
     },
   },
   plugins: [],
-}
+};
