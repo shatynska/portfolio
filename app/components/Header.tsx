e1764a8bced93b0195beddadde2f729333cf8542
Helen Shatynska <helen.shatynska@gmail.com>
2023-05-20 18:22:44 +0300

    Add Header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Cell from "../components/Cell";
+import MainNavigation from "../components/MainNavigation";
+import developerImage from "/public/developer.jpg";
+
+export default function Header() {
+  return (
+    <header className="flex">
+      <Cell className="cell-lg">
+        <Image
+          src={developerImage}
+          alt="Web developer picture"
+          placeholder="blur"
+          className="object-cover"
+          fill
+          priority
+        />
+      </Cell>
+      <Cell className="cell-lg justify-around bg-primary-200 bg-[url('/backgrounds/line.svg')]">
+        <h1 className="text-[2.5rem] font-bold leading-[3.25rem]">
+          Web
+          <br />
+          Developer
+        </h1>
+        <div className="text-right text-[2.5rem] font-bold leading-[3.25rem]">
+          Helen
+          <br />
+          Shatynska
+        </div>
+      </Cell>
+      <MainNavigation />
+    </header>
+  );
+}
