4690f924ea6de7546bc997050e0af90638d92a2f
Helen Shatynska <helen.shatynska@gmail.com>
2023-05-22 14:14:58 +0300

    Hardcode temporary selected projects

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
 import Cell from "./Cell";
 
 export default function Project({ projectId }: { projectId?: number }) {
   return (
     <article className="flex">
-      <Cell className="cell-md bg-primary-200"></Cell>
-      <Cell className="cell-md"></Cell>
-      <Cell className="cell-md bg-primary-800 text-primary-100">
+      <Cell className="cell-md border-b-2 border-primary-100 bg-primary-200">
+        <Image
+          src="/projects/pigeons.svg"
+          alt="Web developer picture"
+          width="220"
+          height="100"
+          className="object-cover"
+        />
+      </Cell>
+      <Cell className="cell-md border-b-2"></Cell>
+      <Cell className="cell-md border-b-2 border-primary-700 bg-primary-800 text-primary-100">
         {projectId}
       </Cell>
     </article>
--- app/components/SelectedProjects.tsx
+++ app/components/SelectedProjects.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Project from "../components/Project";
 import Cell from "../components/Cell";
 
@@ -6,9 +7,74 @@ export default function SelectedProjects() {
 
   return (
     <section>
-      {selectedProjects.map((id) => (
+      {/* {selectedProjects.map((id) => (
         <Project projectId={id} />
-      ))}
+      ))} */}
+
+      <article className="flex">
+        <Cell className="cell-md  items-center border-b-2 border-white bg-primary-200">
+          <Image
+            src="/projects/image.svg"
+            alt="Web developer picture"
+            width="80"
+            height="100"
+            className="object-cover"
+          />
+        </Cell>
+        <Cell className="cell-md border-b-2">
+          <p className="text-sm">Role: UI/UX Designer, Fullstack developer</p>
+          <p className="text-sm">
+            Stack: PHP, Laravel, TypeScript, React, Inertia, Tailwind
+          </p>
+        </Cell>
+        <Cell className="cell-md border-b-2 border-primary-900 bg-primary-800 text-primary-100">
+          <h3 className="text-xl">Drohobych Psychotherapists Union</h3>
+          <p className="text-sm">CMS</p>
+        </Cell>
+      </article>
+
+      <article className="flex">
+        <Cell className="cell-md  items-center border-b-2 border-white bg-primary-200">
+          <Image
+            src="/projects/psy.svg"
+            alt="Web developer picture"
+            width="260"
+            height="100"
+            className="object-cover"
+          />
+        </Cell>
+        <Cell className="cell-md border-b-2">
+          <p className="text-sm">Role: UI/UX Designer, Fullstack developer</p>
+          <p className="text-sm">
+            Stack: PHP, Laravel, React, TypeScript, Tailwind
+          </p>
+        </Cell>
+        <Cell className="cell-md border-b-2 border-primary-800 bg-primary-700 text-primary-100">
+          <h3 className="text-xl">Private psychology practice</h3>
+          <p className="text-sm">Web application</p>
+        </Cell>
+      </article>
+
+      <article className="flex">
+        <Cell className="cell-md  items-center border-b-2 border-white bg-primary-200">
+          <Image
+            src="/projects/pigeons.svg"
+            alt="Web developer picture"
+            width="220"
+            height="100"
+            className="object-cover"
+          />
+        </Cell>
+        <Cell className="cell-md border-b-2">
+          <p className="text-sm">Role: UI/UX Designer, Frontend developer</p>
+          <p className="text-sm">Stack: TypeScript, React, Next.js, Tailwind</p>
+        </Cell>
+        <Cell className="cell-md border-b-2 border-primary-700 bg-primary-600 text-primary-100">
+          <h3 className="text-xl">Web portfolio</h3>
+          <p className="text-sm">Website</p>
+        </Cell>
+      </article>
+
       <section className="flex">
         <Cell className="cell-sm border-r-2"></Cell>
         <Cell className="cell-sm items-center bg-primary-700 text-primary-100">
