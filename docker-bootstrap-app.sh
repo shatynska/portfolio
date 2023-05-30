89c141d795c1a5f87e0483f99bde0c8b096cc303
Helen Shatynska <helen.shatynska@gmail.com>
2023-05-30 23:05:03 +0300

    Replace DATABASE_URL by postgres

--- docker-bootstrap-app.sh
+++ docker-bootstrap-app.sh
@@ -2,6 +2,6 @@
 # ENVIRONEMTN from docker-compose.yaml doesn't get through to subprocesses
 # Need to explicit pass DATABASE_URL here, otherwise migration doesn't work
 # Run migrations
-DATABASE_URL="postgres://shatynska:shatynska@db:5432/appdb?sslmode=disable" npx prisma migrate deploy
+DATABASE_URL="postgres://shatynska:shatynska@postgres:5432/appdb?sslmode=disable" npx prisma migrate deploy
 # start app
-DATABASE_URL="postgres://shatynska:shatynska@db:5432/workler?sslmode=disable" node server.js
+DATABASE_URL="postgres://shatynska:shatynska@postgres:5432/workler?sslmode=disable" node server.js
