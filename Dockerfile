c9396fb79937b9953536d8f7ca05f0bab4b69567
Helen Shatynska <helen.shatynska@gmail.com>
2023-05-31 18:23:24 +0300

    Replace postgres by postgresql

--- Dockerfile
+++ Dockerfile
@@ -59,4 +59,4 @@ EXPOSE 3000
 
 ENV PORT 3000
 
-CMD ["./docker-bootstrap-app.sh"]
+CMD ["/docker-bootstrap-app.sh"]
--- docker-bootstrap-app.sh
+++ docker-bootstrap-app.sh
@@ -2,6 +2,6 @@
 # ENVIRONEMTN from docker-compose.yaml doesn't get through to subprocesses
 # Need to explicit pass DATABASE_URL here, otherwise migration doesn't work
 # Run migrations
-DATABASE_URL="postgres://shatynska:shatynska@185.233.36.64:5432/appdb?sslmode=disable" npx prisma migrate deploy
+DATABASE_URL="postgresql://shatynska:shatynska@185.233.36.64:5432/appdb?sslmode=disable" npx prisma migrate deploy
 # start app
-DATABASE_URL="postgres://shatynska:shatynska@185.233.36.64:5432/workler?sslmode=disable" node server.js
+DATABASE_URL="postgresql://shatynska:shatynska@185.233.36.64:5432/workler?sslmode=disable" node server.js
