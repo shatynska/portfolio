66b65ad22b327ace70fed87eb3de1b9588dc5917
Helen Shatynska <helen.shatynska@gmail.com>
2023-06-06 19:23:27 +0300

    Refactor Project component

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -3,37 +3,39 @@ import Cell from "./Cell";
 import { prisma } from "@/lib/prisma";
 
 export default async function Project({ projectId }: { projectId?: number }) {
-  const project = await prisma.project.findFirst({
+  const project = await prisma.project.findUnique({
     where: {
       id: projectId,
     },
+    include: {
+      roles: {
+        include: {
+          role: true,
+        },
+      },
+    },
   });
   return (
-    <article className="flex flex-col lg:flex-row" key={project?.id}>
-      <Cell className="cell-md items-center border-b-2 border-primary-50 bg-primary-300">
-        <Image
-          src={project?.image || "/default.jpg"}
-          alt={project?.title + "picture"}
-          fill
-          className="object-contain"
-        />
-      </Cell>
-      <Cell className="cell-md bg-primary-800 text-primary-100 lg:order-last">
+    <Cell
+      key={project?.id}
+      className="flex h-144 content-start gap-10 border-primary-50 bg-primary-300 text-sm"
+    >
+      <Image
+        src={project?.image || "/default.jpg"}
+        alt={project?.title + "picture"}
+        width="384"
+        height="192"
+        className="object-contain"
+      />
+      <section>
+        <p>{project?.type}</p>
         <h3>{project?.title}</h3>
-        <p className="text-sm">
-          {/* {project?.types.map((type) => (
-          {type.title}
-          ))} */}
+        <p>
+          Roles:&nbsp;
+          {project?.roles.map((role) => role.role.title + " ")}
         </p>
-      </Cell>
-      <Cell className="cell-md border-b-2 bg-primary-50">
-        <p className="text-sm">
-          {/* {project?.roles.map((role) => (
-          {role.title}
-          ))} */}
-        </p>
-        <p className="text-sm">Stack: {project?.stack}</p>
-      </Cell>
-    </article>
+        <p>Stack: {project?.stack}</p>
+      </section>
+    </Cell>
   );
 }
