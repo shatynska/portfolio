35813237b166b6d5145c4321ee666120da472b86
Helen Shatynska <helen.shatynska@gmail.com>
2023-06-09 02:27:35 +0300

    Add space under CarouselNavigation in mobile

--- app/components/CarouselNavigation.tsx
+++ app/components/CarouselNavigation.tsx
@@ -6,12 +6,12 @@ export default function CarouselNavigation({
   children?: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-row">
+    <section className="flex flex-col md:flex-row">
       <Cell className="hidden h-18 border-l-2 bg-primary-50 md:block"></Cell>
       <Cell className="h-18 items-center bg-primary-700 text-primary-100">
         {children}
       </Cell>
-      <Cell className="hidden h-18 bg-primary-300 lg:block"></Cell>
+      <Cell className="md:hidden h-18 bg-primary-300 lg:block"></Cell>
     </section>
   );
 }
