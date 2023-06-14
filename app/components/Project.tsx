a498ca675ae20ad25a6ef1d4699444f41383867b
Helen Shatynska <helen.shatynska@gmail.com>
2023-06-15 00:25:25 +0300

    Add verifying whether data exist in Project

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -29,14 +29,17 @@ export default async function Project({ projectId }: { projectId?: number }) {
       className="keen-slider__slide flex h-144 divide-y-2 px-0 text-sm"
     >
       <section className="flex h-48 w-full justify-center overflow-hidden pb-8 pt-16">
-        <Image
-          src={project?.image || "/default.jpg"}
-          alt={project?.title + "picture"}
-          width="384"
-          height="192"
-          className="object-contain"
-        />
+        {project?.image && (
+          <Image
+            src={project.image}
+            alt={project?.title + "picture"}
+            width="384"
+            height="192"
+            className="object-contain"
+          />
+        )}
       </section>
+
       <section className="flex h-72 w-full flex-col gap-4 p-10 leading-6 ">
         <div className="h-8">{project?.type}</div>
         <h3 className="flex h-16 items-center overflow-hidden">
@@ -45,25 +48,26 @@ export default async function Project({ projectId }: { projectId?: number }) {
         <div className="h-8 truncate">
           Roles:&nbsp;
           {project?.roles.map((role) => (
-            <span className="after:content-[',\a0'] last:content-none">
+            <span key={role.role.id} className="first:after:content-[',\a0']">
               {role.role.title}
             </span>
           ))}
         </div>
         <div className="h-16 overflow-hidden">Stack: {project?.stack}</div>
       </section>
+
       <section className="flex h-24 items-center  justify-center gap-8 overflow-hidden [&>*]:fill-inherit">
-        <Link href={project?.gitHubUrl || ""} className="[&>*]:fill-inherit">
-          <MoreIcon />
-        </Link>
+        {project?.gitHubUrl && (
+          <Link href={project?.gitHubUrl || ""} className="[&>*]:fill-inherit">
+            <GithubIcon />
+          </Link>
+        )}
 
-        <Link href={project?.gitHubUrl || ""} className="[&>*]:fill-inherit">
-          <GithubIcon />
-        </Link>
-
-        <Link href={project?.gitHubUrl || ""} className="[&>*]:fill-inherit">
-          <WwwIcon />
-        </Link>
+        {project?.url && (
+          <Link href={project.url} className="[&>*]:fill-inherit">
+            <WwwIcon />
+          </Link>
+        )}
       </section>
     </Cell>
   );
