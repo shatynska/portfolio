1a24a3e266ee35b1bbbf4497d741f22bd67b1e42
Helen Shatynska <helen.shatynska@gmail.com>
2023-06-22 23:59:17 +0300

    Change Humburger

--- app/components/Hamburger.tsx
+++ app/components/Hamburger.tsx
@@ -3,19 +3,15 @@ import MainNavigation from "./MainNavigation";
 
 export default function Hamburger() {
   return (
-    <button className="group fixed right-2 top-2 mix-blend-difference hover:opacity-80 lg:hidden">
-      <div className="relative flex h-16 w-16 transform items-center justify-center overflow-hidden transition-all duration-200">
-        <div className="flex h-6 w-7 origin-center transform flex-col justify-around overflow-hidden transition-all duration-300">
-          <div className="h-[3px] w-7 origin-left transform bg-primary-400 transition-all duration-300 group-focus:translate-x-10"></div>
-          <div className="h-[3px] w-7 origin-left transform bg-primary-400 transition-all duration-300 group-focus:translate-x-10"></div>
-          <div className="h-[3px] w-7 transform bg-primary-400 transition-all delay-75 duration-300 group-focus:translate-x-10"></div>
-
-          <div className="absolute top-3 flex w-0 -translate-x-10 transform items-center justify-between transition-all duration-500 group-focus:w-12 group-focus:translate-x-0">
-            <div className="absolute h-[3px] w-7 rotate-0 transform bg-primary-400 transition-all delay-300 duration-500 group-focus:rotate-45"></div>
-            <div className="absolute h-[3px] w-7 -rotate-0 transform bg-primary-400 transition-all delay-300 duration-500 group-focus:-rotate-45"></div>
-          </div>
-        </div>
+    <section className="top-nav">
+      <input id="menu-toggle" type="checkbox" />
+      <label className="menu-button-container" htmlFor="menu-toggle">
+        <div className="menu-button"></div>
+      </label>
+      <div className="menu">
+        <MainNavigation />
       </div>
-    </button>
+      <div className="bg-gray-700 opacity-50 w-full h-full"></div>
+    </section>
   );
 }
