5025cdf350d6fedb0a202a7bc31a1bbd0549c781
Helen Shatynska <helen.shatynska@gmail.com>
2023-06-26 23:53:23 +0300

    Make transformation for Hamburger icon into Cross

--- app/components/Hamburger.tsx
+++ app/components/Hamburger.tsx
@@ -7,22 +7,22 @@ export default function Hamburger() {
       <input id="menu-toggle" className="peer hidden" type="checkbox" />
       <label
         htmlFor="menu-toggle"
-        className="group fixed right-2 top-2 cursor-pointer mix-blend-difference hover:opacity-80"
+        className="fixed right-4 top-4 cursor-pointer mix-blend-difference hover:opacity-80 peer-checked:[&_.burger]:translate-x-10 peer-checked:[&_.cross-line-down]:-rotate-45 peer-checked:[&_.cross-line-up]:rotate-45 peer-checked:[&_.cross]:w-12 peer-checked:[&_.cross]:translate-x-0"
       >
         <div className="relative flex h-16 w-16 transform items-center justify-center overflow-hidden transition-all duration-200">
           <div className="flex h-6 w-7 origin-center transform flex-col justify-around overflow-hidden transition-all duration-300">
-            <div className="h-[3px] w-7 origin-left transform bg-primary-400 transition-all duration-300 group-focus:translate-x-10"></div>
-            <div className="h-[3px] w-7 origin-left transform bg-primary-400 transition-all duration-300 group-focus:translate-x-10"></div>
-            <div className="h-[3px] w-7 transform bg-primary-400 transition-all delay-75 duration-300 group-focus:translate-x-10"></div>
+            <div className="burger h-[3px] w-7 origin-left transform bg-primary-500 transition-all duration-300"></div>
+            <div className="burger h-[3px] w-7 origin-left transform bg-primary-500 transition-all duration-300"></div>
+            <div className="burger h-[3px] w-7 transform bg-primary-500 transition-all delay-75 duration-300"></div>
 
-            <div className="absolute top-3 flex w-0 -translate-x-10 transform items-center justify-between transition-all duration-500 group-focus:w-12 group-focus:translate-x-0">
-              <div className="absolute h-[3px] w-7 rotate-0 transform bg-primary-400 transition-all delay-300 duration-500 group-focus:rotate-45"></div>
-              <div className="absolute h-[3px] w-7 -rotate-0 transform bg-primary-400 transition-all delay-300 duration-500 group-focus:-rotate-45"></div>
+            <div className="cross absolute top-3 flex w-0 -translate-x-10 transform items-center justify-between transition-all duration-500">
+              <div className="cross-line-up absolute h-[3px] w-7 rotate-0 transform bg-primary-400 transition-all delay-300 duration-500"></div>
+              <div className="cross-line-down absolute h-[3px] w-7 -rotate-0 transform bg-primary-400 transition-all delay-300 duration-500"></div>
             </div>
           </div>
         </div>
       </label>
-      <div className="menu absolute -z-10 -mr-[100%] flex w-0 flex-col items-end transition-all duration-300 peer-checked:mr-0 peer-checked:w-full">
+      <div className="absolute -z-10 -mr-[100%] flex w-0 flex-col items-end transition-all duration-300 peer-checked:mr-0 peer-checked:w-full">
         <MainNavigation />
         <div className="absolute left-0 top-0 -z-50 m-0 h-screen w-full bg-primary-900 p-0 opacity-90"></div>
       </div>
