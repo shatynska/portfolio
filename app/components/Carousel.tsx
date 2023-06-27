b6ad6f1201b1895d857c7b2accb2630d679af49e
Helen Shatynska <helen.shatynska@gmail.com>
2023-06-27 23:32:27 +0300

    Remove Arrow component from Carousel component

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 import CarouselNavigation from "../components/CarouselNavigation";
+import Arrow from "./icons/Arrow";
 
 export default function Carousel({
   className,
@@ -112,33 +113,5 @@ export default function Carousel({
         </CarouselNavigation>
       )}
     </div>
-  );
-}
-
-function Arrow(props: {
-  disabled: boolean;
-  left?: boolean;
-  onClick: (e: any) => void;
-}) {
-  const disabeld = props.disabled ? " arrow--disabled" : "";
-  return (
-    <svg
-      onClick={props.onClick}
-      className={`cursor-pointer hover:opacity-80 ${
-        props.left ? "arrow--left" : "arrow--right"
-      } ${disabeld}`}
-      xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
-    >
-      {props.left && (
-        <path d="M14 28L2 16L14 4" stroke="white" stroke-width="3" />
-      )}
-      {!props.left && (
-        <path d="M18 28L30 16L18 4" stroke="white" stroke-width="3" />
-      )}
-    </svg>
   );
 }
