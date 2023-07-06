0bfe62d1df67d9d78139f1395503bcdefc8d847b
Helen Shatynska <helen.shatynska@gmail.com>
2023-07-06 14:38:42 +0300

    Test i18n without Carousel component

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -11,10 +11,11 @@ export default async function Projects() {
   });
 
   return (
-    <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
-      {projects.map((project) => (
-        <Project projectId={project.id} />
-      ))}
-    </Carousel>
+    <div></div>
+    // <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
+    //   {projects.map((project) => (
+    //     <Project projectId={project.id} />
+    //   ))}
+    // </Carousel>
   );
 }
