6604055529bebdde21036318402542812e3244b1
Helen Shatynska <helen.shatynska@gmail.com>
2023-07-18 17:17:50 +0300

    Replace prisma orm with drizzle in Projects component

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,21 +1,16 @@
-// "use client";
-import { prisma } from "@/lib/prisma";
 import Carousel from "../components/Carousel";
 import Project from "../components/Project";
+import { db } from "@/drizzle/";
+import { project } from "@/drizzle/schema";
 
 export default async function Projects() {
-  // const projects = await prisma.project.findMany({
-  //   select: {
-  //     id: true,
-  //   },
-  // });
+  const projects = await db.select().from(project);
 
   return (
-    <div></div>
-    // <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
-    //   {projects.map((project) => (
-    //     <Project projectId={project.id} />
-    //   ))}
-    // </Carousel>
+      <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
+        {projects.map((project) => (
+          <Project projectId={project.id} />
+        ))}
+      </Carousel>
   );
 }
