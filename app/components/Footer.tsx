4707b8f0a08b63f6deae01cba5ad380237c5c81b
Helen Shatynska <helen.shatynska@gmail.com>
2023-07-23 23:01:21 +0300

    Add i18n to Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,18 @@
-import Image from "next/image";
+import { useLocale } from "next-intl";
+import { getTranslator } from "next-intl/server";
+import Link from "next-intl/link";
 import Cell from "../components/Cell";
-import MainNavigation from "./MainNavigation";
 
-export default function Footer() {
+export default async function Footer() {
+  const locale = useLocale();
+  const t = await getTranslator(locale, "Index");
   return (
     <footer>
       <section className="flex flex-col md:h-24 md:flex-row">
         <Cell className="h-24 items-center bg-primary-900 text-base text-primary-100">
-          <button className="font-bo text-lg  hover:opacity-80">
-            Contact me
-          </button>
+          <Link href="/contact" className="text-lg font-semibold">
+            {t("Contact me")}
+          </Link>
         </Cell>
         <Cell className=" bg-primary-300"></Cell>
         <Cell className="hidden bg-primary-50 lg:block"></Cell>
@@ -18,7 +21,7 @@ export default function Footer() {
         <Cell className="hidden bg-primary-300 md:block "></Cell>
         <Cell className="hidden bg-primary-50 lg:block"></Cell>
         <Cell className="items-center bg-primary-600 text-sm text-primary-100">
-          © 2023 Helen Shatynska
+          © 2023 {t("Helen") + "\u00A0" + t("Shatynska")}
         </Cell>
       </section>
     </footer>
