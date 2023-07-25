bcf2b2d393d8e38ff6afa8bad2aa144de0021720
Helen Shatynska <helen.shatynska@gmail.com>
2023-07-25 23:34:01 +0300

    Wrap Carousel with ProjectsCarouselContextProvider

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,3 +1,4 @@
+import { ProjectsCarouselContextProvider } from "@/contexts/ProjectsCarouselContext";
 import Carousel from "../components/Carousel";
 import Project from "../components/Project";
 import { db } from "@/drizzle/";
@@ -7,10 +8,12 @@ export default async function Projects() {
   const projects = await db.select().from(project);
 
   return (
-      <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
-        {projects.map((project) => (
-          <Project projectId={project.id} />
-        ))}
-      </Carousel>
+      <ProjectsCarouselContextProvider>
+        <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
+          {projects.map((project) => (
+            <Project projectId={project.id} />
+          ))}
+        </Carousel>
+      </ProjectsCarouselContextProvider>
   );
 }
