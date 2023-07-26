f6eb9717327d877319098152aefba3297289e5b7
Helen Shatynska <helen.shatynska@gmail.com>
2023-07-26 14:35:25 +0300

    Replace defaultLocale by en in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
   locales: ["en", "ua"],
-  defaultLocale: "ua",
+  defaultLocale: "en",
 });
 
 export const config = {
