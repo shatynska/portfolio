d71d435a938e648c35dffba5136226fadbfcce71
Helen Shatynska <helen.shatynska@gmail.com>
2023-07-29 23:41:24 +0300

    Add relations tables to schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,60 +1,61 @@
-import { pgTable, pgEnum, pgSchema, AnyPgColumn, varchar, timestamp, text, integer, uniqueIndex, serial, foreignKey, primaryKey } from "drizzle-orm/pg-core"
+import {
+  pgTable,
+  serial,
+  text,
+  integer,
+  boolean,
+  primaryKey,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
-
-import { sql } from "drizzle-orm"
-
-export const prismaMigrations = pgTable("_prisma_migrations", {
-	id: varchar("id", { length: 36 }).primaryKey().notNull(),
-	checksum: varchar("checksum", { length: 64 }).notNull(),
-	finishedAt: timestamp("finished_at", { withTimezone: true, mode: 'string' }),
-	migrationName: varchar("migration_name", { length: 255 }).notNull(),
-	logs: text("logs"),
-	rolledBackAt: timestamp("rolled_back_at", { withTimezone: true, mode: 'string' }),
-	startedAt: timestamp("started_at", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
-	appliedStepsCount: integer("applied_steps_count").notNull(),
+export const projects = pgTable("projects", {
+  id: serial("id").primaryKey().notNull(),
+  title: text("title").notNull(),
+  type: text("type").notNull(),
+  stack: text("stack").notNull(),
+  url: text("url"),
+  gitHubUrl: text("gitHubUrl"),
+  image: text("image"),
 });
 
-export const role = pgTable("Role", {
-	id: serial("id").primaryKey().notNull(),
-	title: text("title").notNull(),
-},
-(table) => {
-	return {
-		titleKey: uniqueIndex("Role_title_key").on(table.title),
-	}
+export const projectsRelations = relations(projects, ({ many }) => ({
+  projectsToRoles: many(projectsToRoles),
+}));
+
+export const roles = pgTable("roles", {
+  id: serial("id").primaryKey().notNull(),
+  title: text("title").notNull(),
 });
 
-export const project = pgTable("Project", {
-	id: serial("id").primaryKey().notNull(),
-	title: text("title").notNull(),
-	type: text("type").notNull(),
-	stack: text("stack").notNull(),
-	url: text("url"),
-	gitHubUrl: text("gitHubUrl"),
-	image: text("image"),
-});
+export const rolesRelations = relations(roles, ({ many }) => ({
+  projectsToRoles: many(projectsToRoles),
+}));
 
-export const topic = pgTable("Topic", {
-	id: serial("id").primaryKey().notNull(),
-	title: text("title").notNull(),
-	projectOnRolesId: integer("projectOnRolesId").notNull().references(() => projectsOnRoles.id, { onDelete: "restrict", onUpdate: "cascade" } ),
-},
-(table) => {
-	return {
-		titleKey: uniqueIndex("Topic_title_key").on(table.title),
-	}
-});
+export const projectsToRoles = pgTable(
+  "projects_to_roles",
+  {
+    projectId: integer("project_id")
+      .notNull()
+      .references(() => projects.id),
+    roleId: integer("role_id")
+      .notNull()
+      .references(() => roles.id),
+  },
+  (t) => ({
+    pk: primaryKey(t.projectId, t.roleId),
+  })
+);
 
-export const projectsOnRoles = pgTable("ProjectsOnRoles", {
-	id: serial("id").notNull(),
-	projectId: integer("projectId").notNull().references(() => project.id, { onDelete: "restrict", onUpdate: "cascade" } ),
-	roleId: integer("roleId").notNull().references(() => role.id, { onDelete: "restrict", onUpdate: "cascade" } ),
-	description: text("description"),
-	images: text("images").array(),
-},
-(table) => {
-	return {
-		idKey: uniqueIndex("ProjectsOnRoles_id_key").on(table.id),
-		projectsOnRolesPkey: primaryKey(table.projectId, table.roleId)
-	}
-});
+export const projectsToRolesRelations = relations(
+  projectsToRoles,
+  ({ one }) => ({
+    project: one(projects, {
+      fields: [projectsToRoles.projectId],
+      references: [projects.id],
+    }),
+    role: one(roles, {
+      fields: [projectsToRoles.roleId],
+      references: [roles.id],
+    }),
+  })
+);
