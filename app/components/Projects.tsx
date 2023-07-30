1583bc06a208c36c2c55fb7b1a882bf73dbeb725
Helen Shatynska <helen.shatynska@gmail.com>
2023-07-30 22:12:51 +0300

    Fix name of db in Projects component

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,15 +2,15 @@ import { ProjectsCarouselContextProvider } from "@/contexts/ProjectsCarouselCont
 import Carousel from "../components/Carousel";
 import Project from "../components/Project";
 import { db } from "@/drizzle/";
-import { project } from "@/drizzle/schema";
+import { projects } from "@/drizzle/schema";
 
 export default async function Projects() {
-  const projects = await db.select().from(project);
+  const allProjects = await db.select().from(projects);
 
   return (
     <ProjectsCarouselContextProvider>
       <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
-        {projects.map((project) => (
+        {allProjects.map((project) => (
           <Project projectId={project.id} />
         ))}
       </Carousel>
