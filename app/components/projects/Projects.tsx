9e6b2e9923f99590587bb92da14e57aa6c94ee65
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-03 21:09:10 +0300

    Refactor try/catch in Projects component

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -3,26 +3,19 @@ import Carousel from "./Carousel";
 import Project from "./Project";
 import { db } from "@/drizzle/";
 import { projects } from "@/drizzle/schema";
-import { notFound } from "next/navigation";
 
 export default async function Projects() {
-  let allProjects;
-
   try {
-    allProjects = await db.select().from(projects);
-  } catch {}
+    const allProjects = await db.select().from(projects);
 
-  return (
-    <>
-      {allProjects && (
-        <ProjectsCarouselContextProvider>
-          <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
-            {allProjects.map((project) => (
-              <Project projectId={project.id} />
-            ))}
-          </Carousel>
-        </ProjectsCarouselContextProvider>
-      )}
-    </>
-  );
+    return (
+      <ProjectsCarouselContextProvider>
+        <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
+          {allProjects.map((project) => (
+            <Project projectId={project.id} />
+          ))}
+        </Carousel>
+      </ProjectsCarouselContextProvider>
+    );
+  } catch {}
 }
