fcae56286ddae48075161aa64324098137203e71
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-03 17:28:14 +0300

    Use try/catch if projects table is not exit

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -3,17 +3,26 @@ import Carousel from "./Carousel";
 import Project from "./Project";
 import { db } from "@/drizzle/";
 import { projects } from "@/drizzle/schema";
+import { notFound } from "next/navigation";
 
 export default async function Projects() {
-  const allProjects = await db.select().from(projects);
+  let allProjects;
+
+  try {
+    allProjects = await db.select().from(projects);
+  } catch {}
 
   return (
-    <ProjectsCarouselContextProvider>
-      <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
-        {allProjects.map((project) => (
-          <Project projectId={project.id} />
-        ))}
-      </Carousel>
-    </ProjectsCarouselContextProvider>
+    <>
+      {allProjects && (
+        <ProjectsCarouselContextProvider>
+          <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
+            {allProjects.map((project) => (
+              <Project projectId={project.id} />
+            ))}
+          </Carousel>
+        </ProjectsCarouselContextProvider>
+      )}
+    </>
   );
 }
