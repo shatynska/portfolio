83872be601ac3e0ff37f92ce02f2c7bc43c64665
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-03 18:55:59 +0300

    Replace .env file by .env.local

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: ".env" });
+dotenv.config();
 
 export default {
   schema: "./drizzle/schema/*",
