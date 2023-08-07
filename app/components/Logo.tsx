212b52640e0c971c18f20e2e9e6f742d0c8a61ac
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-07 23:05:20 +0300

    Add link to home page in Logo component

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -2,19 +2,22 @@ import { useLocale } from "next-intl";
 import { getTranslator } from "next-intl/server";
 import Image from "next/image";
 import logoImage from "/public/logo.svg";
+import Link from "next/link";
 
 export default async function Logo() {
   const locale = useLocale();
   const t = await getTranslator(locale, "Index");
 
   return (
-    <div className="m-4 flex gap-2">
-      <Image src={logoImage} alt={t("logo")} height={40} width={40} />
-      <div>
-        {t("Web")}
-        <br />
-        {t("Developer")}
+    <Link href="/" title={t("Home page")}>
+      <div className="m-4 flex gap-2">
+        <Image src={logoImage} alt={t("logo")} height={40} width={40} />
+        <div>
+          {t("Web")}
+          <br />
+          {t("Developer")}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
