c1c0da7305b91b477a7e0d5e084c40a464694223
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-11 22:17:26 +0300

    Create HeaderForInnerPage component

---
+++ app/components/HeaderForInnerPage.tsx
@@ -0,0 +1,14 @@
+import Cell from "./Cell";
+import Logo from "./Logo";
+
+export default async function HeaderForInnerPage() {
+  return (
+    <header className="flex flex-col md:h-24 md:flex-row">
+      <Cell className="h-24 flex-row gap-4 bg-primary-900 text-sm text-primary-100">
+        <Logo />
+      </Cell>
+      <Cell className="hidden bg-primary-50 md:block"></Cell>
+      <Cell className="hidden bg-primary-200 lg:block"></Cell>
+    </header>
+  );
+}
