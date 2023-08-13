031b2ce09fcf45a2df9cce4fd9ad0abd43627c5a
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-13 23:57:43 +0300

    Add check pathname in Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,22 +1,18 @@
-import { useLocale } from "next-intl";
-import { getTranslator } from "next-intl/server";
-import Link from "next-intl/link";
+"use client";
+
+import { useTranslations } from "next-intl";
+import { usePathname } from "next/navigation";
 import Cell from "../components/Cell";
+import ContactMeSection from "./ContactMeSection";
 
 export default async function Footer() {
-  const locale = useLocale();
-  const t = await getTranslator(locale, "Index");
+  const t = useTranslations("Index");
+
+  const pathname = usePathname();
+
   return (
     <footer>
-      <section className="flex flex-col md:h-24 md:flex-row">
-        <Cell className="h-24 items-center bg-primary-900 text-base text-primary-100">
-          <Link href="/contact" className="text-lg font-semibold">
-            {t("Contact me")}
-          </Link>
-        </Cell>
-        <Cell className="bg-primary-300" />
-        <Cell className="order-first border border-primary-200 bg-primary-50 md:hidden lg:order-none lg:block" />
-      </section>
+      {pathname !== "/contact" && <ContactMeSection />}
       <section className="flex flex-col md:h-24 md:flex-row">
         <Cell className="hidden bg-primary-300 md:block " />
         <Cell className="hidden border border-primary-200 bg-primary-50 lg:block" />
