e8916ac7ac861e5fb285f0d9fc04712a3fa7f2b3
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-14 00:21:20 +0300

    Add check of  allProjects array length

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -8,14 +8,16 @@ export default async function Projects() {
   try {
     const allProjects = await db.select().from(projects);
 
-    return (
-      <ProjectsCarouselContextProvider>
-        <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
-          {allProjects.map((project) => (
-            <Project projectId={project.id} />
-          ))}
-        </Carousel>
-      </ProjectsCarouselContextProvider>
-    );
+    if (allProjects.length != 0) {
+      return (
+        <ProjectsCarouselContextProvider>
+          <Carousel className="flex w-screen max-w-112 flex-col overflow-hidden md:w-192 md:max-w-none lg:w-288 ">
+            {allProjects.map((project) => (
+              <Project projectId={project.id} />
+            ))}
+          </Carousel>
+        </ProjectsCarouselContextProvider>
+      );
+    }
   } catch {}
 }
