699027c0af061a10164eedb1cb70cccc1c1578af
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-13 23:55:57 +0300

    Move contactMe section into separate container

---
+++ app/components/ContactMeSection.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import Cell from "./Cell";
+
+export default function ContactMeSection() {
+  const t = useTranslations("Index");
+
+  return (
+    <section className="flex flex-col md:h-24 md:flex-row">
+      <Cell className="h-24 items-center bg-primary-900 text-base text-primary-100">
+        <Link href="/contact" className="text-lg font-semibold">
+          {t("Contact me")}
+        </Link>
+      </Cell>
+      <Cell className="bg-primary-300" />
+      <Cell className="order-first border border-primary-200 bg-primary-50 md:hidden lg:order-none lg:block" />
+    </section>
+  );
+}
