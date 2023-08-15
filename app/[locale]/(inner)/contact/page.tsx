c18a9fd2191a340218b46bf2e675c47cb1389d4e
Helen Shatynska <helen.shatynska@gmail.com>
2023-08-15 23:59:04 +0300

    Create base for contact page

---
+++ app/[locale]/(inner)/contact/page.tsx
@@ -0,0 +1,8 @@
+import HeaderForInnerPage from "@/app/components/HeaderForInnerPage";
+import PrimaryHeading from "@/app/components/PrimaryHeading";
+import SecondaryHeading from "@/app/components/SecondaryHeading";
+import Project from "@/app/components/projects/Project";
+
+export default function Page() {
+  return <></>;
+}
