f02f64859c1d08f38429f17904140b28ed422605
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-21 22:35:21 +0300

    Init shadcn-ui

---
+++ tailwind.config.ts
@@ -0,0 +1,114 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        md: "768px",
+        lg: "1152px",      
+      },
+    },
+    extend: {
+      spacing: {
+        18: "4.5rem",
+        112: "28rem",
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+        192: "48rem",
+        288: "72rem",
+      },
+      minHeight: {
+        16: "4rem",
+        24: "6rem",
+        128: "32rem",
+      },
+      maxWidth: {
+        96: "24rem",
+        112: "28rem",
+        288: "72rem",
+      },
+      minWidth: {
+        96: "24rem",
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+          50: "#FFFFFF",
+          100: "#FAFAFA",
+          200: "#F8F8F8",
+          300: "#F5F5F5",
+          400: "#BBBBBB",
+          500: "#888888",
+          600: "#606060",
+          700: "#3A3A3A",
+          800: "#303030",
+          900: "#222222",
+          950: "#111111",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
