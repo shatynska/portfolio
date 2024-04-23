081ee6bc8c84a8b5a1f4959aa254139ef9dee274
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-24 00:03:24 +0300

    Separate social media links into separate component

--- src/components/navigation/MainNavigation.tsx
+++ src/components/navigation/MainNavigation.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import Cell from '../Cell';
 import ContactLinks from './ContactLinks';
 import I18nLinks from './I18nLinks';
 import MainLinks from './MainLinks';
+import SocialMediaLinks from './SocialMediaLinks';
 
 export default function MainNavigation({ className }: { className?: string }) {
   return (
@@ -11,32 +10,7 @@ export default function MainNavigation({ className }: { className?: string }) {
       <I18nLinks />
       <ContactLinks />
       <MainLinks />
-      <div className="flex h-24 items-center justify-center gap-8  ">
-        <Link
-          href="https://www.linkedin.com/in/shatynska/"
-          target="_blank"
-          title="Linkedin profile"
-        >
-          <Image
-            src="/icons/linkedin.svg"
-            height="32"
-            width="32"
-            alt="Linkedin icon"
-          />
-        </Link>
-        <Link
-          href="https://github.com/shatynska"
-          target="_blank"
-          title="GitHub profile"
-        >
-          <Image
-            src="/icons/github.svg"
-            height="32"
-            width="32"
-            alt="GitHub icon"
-          />
-        </Link>
-      </div>
+      <SocialMediaLinks />
     </Cell>
   );
 }
---
+++ src/components/navigation/SocialMediaLinks.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function SocialMediaLinks() {
+  return (
+    <div className="flex h-24 items-center justify-center gap-8  ">
+      <Link
+        href="https://www.linkedin.com/in/shatynska/"
+        target="_blank"
+        title="Linkedin profile"
+      >
+        <Image
+          src="/icons/linkedin.svg"
+          height="32"
+          width="32"
+          alt="Linkedin icon"
+        />
+      </Link>
+      <Link
+        href="https://github.com/shatynska"
+        target="_blank"
+        title="GitHub profile"
+      >
+        <Image
+          src="/icons/github.svg"
+          height="32"
+          width="32"
+          alt="GitHub icon"
+        />
+      </Link>
+    </div>
+  );
+}
