9a8144d0077c8646fef014b6c85f9219318617cb
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-24 02:15:31 +0300

    Add popover component from shadcn and use in in ContactMeSection

--- src/components/ContactMeSection.tsx
+++ src/components/ContactMeSection.tsx
@@ -1,16 +1,23 @@
-import { Link } from '@/navigation';
 import { useTranslations } from 'next-intl';
 import Cell from './Cell';
+import ContactLinks from './navigation/ContactLinks';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 
 export default function ContactMeSection() {
   const t = useTranslations('Index');
 
   return (
     <section className="flex flex-col md:flex-row">
-      <Cell background="darker" className="items-center text-base">
-        <Link href="/contact" className="text-lg font-semibold">
-          {t('Contact me')}
-        </Link>
+      <Cell
+        background="darker"
+        className="items-center text-base font-semibold"
+      >
+        <Popover>
+          <PopoverTrigger>{t('Contact me')}</PopoverTrigger>
+          <PopoverContent>
+            <ContactLinks />
+          </PopoverContent>
+        </Popover>
       </Cell>
       <Cell background="light" />
       <Cell className="order-first border md:hidden lg:order-none lg:block" />
