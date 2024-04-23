fc368109931a553563153fd1e7589629a23e4f88
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-24 00:01:47 +0300

    Replace skype with whatsapp in ContactLinks

--- src/components/navigation/ContactLinks.tsx
+++ src/components/navigation/ContactLinks.tsx
@@ -1,11 +1,24 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function ContactLinks() {
   return (
     <div className="flex h-24 items-center justify-center gap-8  ">
       <Link href="tel:+380978976847" target="_blank" title="Phone number">
         <Image src="/icons/phone.svg" height="32" width="32" alt="Phone icon" />
+      </Link>
+
+      <Link
+        href="https://wa.me/380978976847"
+        target="_blank"
+        title="Whatsapp chat"
+      >
+        <Image
+          src="/icons/whatsapp.svg"
+          height="32"
+          width="32"
+          alt="Whatsapp icon"
+        />
       </Link>
       <Link
         href="mailto:helenshatynska@gmail.com"
@@ -18,13 +31,6 @@ export default function ContactLinks() {
           width="32"
           alt="E-mail icon"
         />
-      </Link>
-      <Link
-        href="skype:shatynskaa?userinfo"
-        target="_blank"
-        title="Skype profile"
-      >
-        <Image src="/icons/skype.svg" height="32" width="32" alt="Skype icon" />
       </Link>
       <Link
         href="https://goo.gl/maps/2d9j9b1gmRQEJgwT6"
