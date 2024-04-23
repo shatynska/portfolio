bcbfbb5b5b80ca968370950f3a5868a13a7e5d3a
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-23 09:51:51 +0300

    Retrieve current year dynamically in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,6 +9,7 @@ export default function Footer() {
   const t = useTranslations('Index');
 
   const pathname = usePathname();
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer>
@@ -17,7 +18,7 @@ export default function Footer() {
         <Cell className="hidden bg-primary-300 md:block " />
         <Cell className="hidden border border-primary-200 bg-primary-50 lg:block" />
         <Cell className="items-center bg-primary-600 text-sm text-primary-100">
-          © 2023 {t('Helen') + '\u00A0' + t('Shatynska')}
+          © {currentYear} {t('Helen') + '\u00A0' + t('Shatynska')}
         </Cell>
       </section>
     </footer>
